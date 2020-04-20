9b3292e8555b0ee4aba248bdb988d56b6eacb008
Chaitanya Naik <chaitanyauday.naik@sjsu.edu>
2020-04-19 21:59:55 -0700

    Code for changing text and href

--- index.js
+++ index.js
@@ -1,5 +1,39 @@
 let links;
 
+class ElementHandler {
+  element(element) {
+    // An incoming element, such as `div`
+    console.error(`Incoming element: ${element.tagName}`)
+
+    if (element.tagName == 'title')
+      element.setInnerContent("Cloudflare Task")
+    else if (element.getAttribute('id') == "description")
+      element.setInnerContent("This is a sample description modified via ElementHandler class");
+    else if (element.getAttribute('id') == "title")
+      element.setInnerContent("Cloudflare Fullstack Internship Task!");
+    else if (element.getAttribute('id') == "url")
+      element.setInnerContent("Check My LinkedIn!");
+  }
+}
+
+class AttributeRewriter {
+  constructor(attributeName) {
+    this.attributeName = attributeName;
+  }
+
+  element(element) {
+    if (this.attributeName == "href")
+      element.setAttribute(this.attributeName, attribute.replace('https://cloudflare.com', 'https://www.linkedin.com/in/chets619/'));
+  }
+}
+
+let rewriter = new HTMLRewriter()
+  .on('h1#title', new ElementHandler())
+  .on('p#description', new ElementHandler())
+  .on('title', new ElementHandler())
+  .on('a#url', new AttributeRewriter("href"))
+  .on('a#url', new ElementHandler());
+
 addEventListener('fetch', event => {
   event.respondWith(handleRequest(event.request))
 })
@@ -13,6 +47,6 @@ async function handleRequest(request) {
   let urlLists = await fetch("https://cfw-takehome.developers.workers.dev/api/variants");
 
   links = await urlLists.json();
-
-  return fetch(links.variants[Math.floor(Math.random() * 2)]);
+  let a = await fetch(links.variants[Math.floor(Math.random() * 2)]);
+  return rewriter.transform(a);
 }
